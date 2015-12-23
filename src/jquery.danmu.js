59005777f2601151472fb0f8cc3a016039b4bb6a
毛姆 <fedeoo.zf@alibaba-inc.com>
2015-12-23 15:32:32 +0800

    fix 循环弹幕条件判断sumTime为undefined

--- src/jquery.danmu.js
+++ src/jquery.danmu.js
@@ -250,6 +250,7 @@ var cyntax = {
         $(element).data("nowTime", 0);
         $(element).data("danmuList", options.danmuList);
         $(element).data("opacity", options.opacity);
+        $(element).data("sumTime", options.sumTime);
         $(element).data("paused", 1);
         $(element).data("topSpace", 0);
         $(element).data("bottomSpace", 0);
